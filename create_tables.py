70dc513f08d2f7c72c3522042be7320a974c4ae4
Nguyen Van Duc <vanduc.dn@gmail.com>
2019-05-06 06:12:51 +0700

    Test on Jupyter Notebook

--- create_tables.py
+++ create_tables.py
@@ -19,11 +19,31 @@ def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(
+        *config['CLUSTER'].values()))
     cur = conn.cursor()
-
-    drop_tables(cur, conn)
-    create_tables(cur, conn)
+    cur.execute(""" 
+                CREATE TABLE staging_events(
+                artist varchar(100) NULL,
+                auth varchar(50) NULL,
+                first_name varchar(50) NULL,
+                last_name varchar(50) NULL,
+                length float NULL,
+                level varchar(50) NULL,
+                location varchar(250) NULL,
+                method varchar(50) NULL,
+                page varchar(50) NULL,
+                registration decimal(16,2) NULL,
+                session_id int NULL,
+                song varchar(250) NULL,
+                status int NULL,
+                ts int NULL,
+                user_agent varchar(500) NULL,
+                user_id varchar(50) NULL)
+                """)
+    conn.commit()
+#     drop_tables(cur, conn)
+#     create_tables(cur, conn)
 
     conn.close()
 
--- sql_queries.py
+++ sql_queries.py
@@ -7,26 +7,47 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplay"
+user_table_drop = "DROP TABLE IF EXISTS user"
+song_table_drop = "DROP TABLE IF EXISTS song"
+artist_table_drop = "DROP TABLE IF EXISTS artist"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-staging_events_table_create= ("""
+staging_events_table_create = ("""
+CREATE TABLE staging_events(
+    artist varchar(100) NULL,
+    auth varchar(50) NULL,
+    first_name varchar(50) NULL,
+    last_name varchar(50) NULL,
+    length float NULL,
+    level varchar(50) NULL,
+    location varchar(250) NULL,
+    method varchar(50) NULL,
+    page varchar(50) NULL,
+    registration decimal(16,2) NULL,
+    session_id int NULL,
+    song varchar(250) NULL,
+    status int NULL,
+    ts int NULL,
+    user_agent varchar(500) NULL,
+    user_id
+)
 """)
 
 staging_songs_table_create = ("""
+
 """)
 
 songplay_table_create = ("""
+
 """)
 
 user_table_create = ("""
+
 """)
 
 song_table_create = ("""
@@ -41,6 +62,8 @@ time_table_create = ("""
 # STAGING TABLES
 
 staging_events_copy = ("""
+COPY staging_events from '{}'
+credential 'aws_iam_role={}'
 """).format()
 
 staging_songs_copy = ("""
@@ -65,7 +88,10 @@ time_table_insert = ("""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [staging_events_table_create, staging_songs_table_create,
+                        songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop,
+                      songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+insert_table_queries = [songplay_table_insert, user_table_insert,
+                        song_table_insert, artist_table_insert, time_table_insert]
