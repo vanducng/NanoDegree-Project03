6af30dadd851518ed8a82bddc08c1d2c694be804
Nguyen Van Duc <vanduc.dn@gmail.com>
2019-05-18 22:44:16 +0700

    Update readme

--- etl.py
+++ etl.py
@@ -4,12 +4,18 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Copy data from json log into staging tables
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Insert data from staging tables into dimensional and fact tables
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
